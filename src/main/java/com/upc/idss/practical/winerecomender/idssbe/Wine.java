f75a1de7cba9607963491dbfb158795595b7e514
Mrvys <mrvy13@gmail.com>
2019-01-11 00:35:18 +0100

    communication with python

--- src/main/java/com/upc/idss/practical/winerecomender/idssbe/Wine.java
+++ src/main/java/com/upc/idss/practical/winerecomender/idssbe/Wine.java
@@ -1,22 +1,26 @@
 package com.upc.idss.practical.winerecomender.idssbe;
 
 public class Wine {
-    private int country;
+    private String country;
     private String keywords;
+    private String priceMin;
+    private String priceMax;
 
-    public Wine(int country, String keywords) {
+    public Wine(String country, String keywords, String priceMin, String priceMax) {
         this.country = country;
         this.keywords = keywords;
+        this.priceMin = priceMin;
+        this.priceMax = priceMax;
     }
 
     public Wine() {
     }
 
-    public int getCountry() {
+    public String getCountry() {
         return country;
     }
 
-    public void setCountry(int country) {
+    public void setCountry(String country) {
         this.country = country;
     }
 
@@ -26,5 +30,21 @@ public class Wine {
 
     public void setKeywords(String keywords) {
         this.keywords = keywords;
+    }
+
+    public String getPriceMin() {
+        return priceMin;
+    }
+
+    public void setPriceMin(String priceMin) {
+        this.priceMin = priceMin;
+    }
+
+    public String getPriceMax() {
+        return priceMax;
+    }
+
+    public void setPriceMax(String priceMax) {
+        this.priceMax = priceMax;
     }
 }
--- src/main/java/com/upc/idss/practical/winerecomender/idssbe/WineController.java
+++ src/main/java/com/upc/idss/practical/winerecomender/idssbe/WineController.java
@@ -4,13 +4,53 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.json.simple.JSONObject;
+
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping({"/api"})
 public class WineController {
 
     @RequestMapping("/keywords")
-    public int create(@RequestBody Wine wine){
-        return wine.getCountry();
+    public JSONObject create(@RequestBody Wine wine){
+        JSONObject obj = new JSONObject();
+        wine.setKeywords(wine.getKeywords().trim().replaceAll("[ ]{1,}", ";"));
+        System.out.println(wine.getKeywords());
+        String arguments = wine.getKeywords() + ' ' + wine.getPriceMin() + ' ' + wine.getPriceMax();
+//        System.out.print(arguments);
+        String result = "";
+        ProcessBuilder builder = new ProcessBuilder(
+                "cmd.exe", "/c", "C:\\Users\\filip\\AppData\\Local\\Programs\\Python\\Python36\\python3.exe C:\\Users\\filip\\Desktop\\UPC\\IDSS\\Project\\IDSS-py\\source\\ranking.py " + arguments);
+        builder.redirectErrorStream(true);
+        Process p = null;
+        try {
+            p = builder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line = "";
+        while (true) {
+            try {
+                line = r.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (line == null) { break; }
+            System.out.println(line);
+            result += line;
+            result += ";";
+        }
+        System.out.println("Finished!");
+        System.out.print(result);
+        wine.setCountry(result);
+        obj.put("result", result);
+
+        return obj;
     }
 }
