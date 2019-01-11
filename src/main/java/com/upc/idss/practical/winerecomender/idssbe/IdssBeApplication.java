5f6b76778ba43e62cb08f07692c641f9943fd5c3
Arild Dalsgård <arild.dalsgaard@gmail.com>
2019-01-11 17:58:45 +0100

    Cleaned the formating of the filepaths.

--- src/main/java/com/upc/idss/practical/winerecomender/idssbe/IdssBeApplication.java
+++ src/main/java/com/upc/idss/practical/winerecomender/idssbe/IdssBeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IdssBeApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(IdssBeApplication.class, args);
 	}
 }
